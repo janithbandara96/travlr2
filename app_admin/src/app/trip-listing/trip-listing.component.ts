7631d5c2c07b6124f6fd76f58a1d1056a4f55df6
tejas-iot <robert@tejasautomation.com>
2021-06-27 15:28:11 +0530

    Create Trip Data Service

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import { trips } from '../data/trips';
-import { TripDataService } from '../services/trip-data.service';
-import { Trip } from '../models/trip';
+import { TripDataService } from '../../../services/trip-data.service';
+import { Trip } from '../../../models/trip';
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
